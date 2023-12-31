723d3fe8dfacfbfae1ab480985148b8b2e7584d6
Diego Alfonso <dlalfonso57@gmail.com>
2023-12-30 21:44:13 -0500

    four commit

--- fs.js
+++ fs.js
@@ -1,9 +1,35 @@
 const fs = require("fs");
 
-fs.readFile("index.html", "utf-8", (err, contenido) => {
+// fs.readFile("index.html", "utf-8", (err, contenido) => {
+//   if (err) {
+//     throw e
+//   } else {
+//     console.log(contenido);
+//   }
+//   console.log("Mensaje...");
+// });
+
+// fs.rename("index.html", "main.html", (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Nombre cambiado exitosamente");
+// });
+
+// agregar contenido al final de una lista
+
+// fs.appendFile("index.html", "<p>Hello</p>", (err) => {
+//   if (err) {
+//     throw err;
+//   }
+//   console.log("Archivo actualizado...");
+// });
+
+//Reemplar todo el contenido del archivo
+
+fs.writeFile("index.html", "contenido nuevo", (err) => {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(contenido);
+    throw err;
   }
+  console.log("Contenido actualizado...");
 });
